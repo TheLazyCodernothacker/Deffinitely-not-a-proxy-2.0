4dddee17f4f80431bf89045c0f7456457f830b75
Lightningeek <117481654+Lightningeek@users.noreply.github.com>
2023-01-07 19:00:41 -0800

    ok global js variables?

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function playMusic(){
   var xMusic = new Audio('Assets/sounds/coolsong.mp3');
+  var yMusic = new Audio('Assets/sounds/Studio_Project.mp3');
+function playMusic(){
   xMusic.play();
   }
 function rickroll(){
-  var yMusic = new Audio('Assets/sounds/Studio_Project.mp3');
   yMusic.play();
   }
 
