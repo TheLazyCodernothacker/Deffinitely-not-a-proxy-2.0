eaf708dfff0c301e8b8aeb942d161cfdfb245c4a
Lightningeek <117481654+Lightningeek@users.noreply.github.com>
2023-01-08 13:37:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
   var xMusic = new Audio('Assets/sounds/coolsong.mp3');
   var yMusic = new Audio('Assets/sounds/Studio_Project.mp3');
   var Nugget = new Audio('Assets/sounds/Nugget.mp3');
-  var celery = new Audio('Assets/sounds/Celebrate.mp3');
-  var SauceBad = new Audio('Assets/sounds/dance.mp3');
+  var celery = new Audio('Assets/sounds/dance.mp3');
+  var SauceBad = new Audio('Assets/sounds/boo.mp3');
 function playMusic(){
   xMusic.play();
   }
