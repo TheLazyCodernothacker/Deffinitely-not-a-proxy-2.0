ff3b0b425ac9fae386f0ad2b64a0da6fd6f18f2d
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-01-09 09:25:57 -0800

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,41 @@
+  var oneMusic = new Audio('Assets/sounds/byob.mp3');
+  var twoMusic = new Audio('Assets/sounds/.mp3');
+  var threeMusic = new Audio('Assets/sounds/.mp3');
+  var fourMusic = new Audio('Assets/sounds/furelise.mp3');
+  var fiveMusic = new Audio('Assets/sounds/.mp3');
+  var sixMusic = new Audio('Assets/sounds/toxicity.mp3');
+  var sevenMusic = new Audio('Assets/sounds/.mp3');
+  var eightMusic = new Audio('Assets/sounds/.mp3');
+  var nineMusic = new Audio('Assets/sounds/.mp3');
+  var tenMUsic = new Audio('Assets/sounds/.mp3');
+function songone(){
+  oneMusic.play();
+  }
+function songtwo(){
+  twoMusic.play();
+  }
+function songthree(){
+  threeMusic.play();
+  }
+function songfour(){
+  fourMusic.play();
+  }
+function songfive(){
+  fiveMusic.play();
+  }
+function songsix(){
+  sixMusic.play();
+  }
+function songseven(){
+  sevenMusic.play();
+  }
+function songeight(){
+  eightMusic.play();
+  }  
+function songnine(){
+  nineMusic.play();
+  }  
+function songten(){
+  tenMusic.play();
+  }    
+  
