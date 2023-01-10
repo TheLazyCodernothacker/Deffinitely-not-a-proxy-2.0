ddf1a1f3ca057e4f89ec2fc2b7c0beaa7ca9d1ba
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-01-10 10:23:52 -0800

    Update songs.js

--- songs.js
+++ songs.js
@@ -2,9 +2,9 @@
   var twoMusic = new Audio('Assets/sounds/smoothjazz.mp3');
   var threeMusic = new Audio('Assets/sounds/In The Hall Of The Mountain King but i got distracted.mp3');
   var fourMusic = new Audio('Assets/sounds/furelise.mp3');
-  var fiveMusic = new Audio('Assets/sounds/.mp3');
+  var fiveMusic = new Audio('Assets/sounds/eruption.mp3');
   var sixMusic = new Audio('Assets/sounds/toxicity.mp3');
-  var sevenMusic = new Audio('Assets/sounds/.mp3');
+  var sevenMusic = new Audio('Assets/sounds/smb.mp3');
   var eightMusic = new Audio('Assets/sounds/amongus.mp3');
   var nineMusic = new Audio('Assets/sounds/RUSH E.mp3');
   var tenMusic = new Audio('Assets/sounds/spirit.mp3');
